20034765e1233ab5d2456ae9231f44cf667552d6
Mia Rivera <riverami@iupui.edu>
2017-07-06 14:58:33 -0400

    "Form updates notes and creates new notes"

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -2,34 +2,45 @@ import React from 'react'
 import './NoteForm.css'
 
 class NoteForm extends React.Component {
-    render() {
-      const {currentNote} = this.props
 
-      return (
-        <div className="NoteForm">
-          <div className="form-actions">
-            <button type="button">
-              <i className="fa fa-trash-o"></i>
-            </button>
-          </div>
-          <form>
-            <p>
-              <input
-                type="text"
-                name="title"
-                placeholder="Title your note"
-                value={currentNote.title}
-              />
-            </p>
-            
-            <textarea 
-              name="body"
-              value={this.props.currentNote.body}>
-            </textarea>
-          </form>
+  handleChanges = (ev) => {
+    const note = {...this.props.currentNote}
+    note[ev.target.name] = ev.target.value
+
+    this.props.saveNote(note)
+  }
+
+  render() {
+    const {currentNote} = this.props
+
+    return (
+      <div className="NoteForm">
+        <div className="form-actions">
+          <button type="button">
+            <i className="fa fa-trash-o"></i>
+          </button>
         </div>
-      )
-    }
+        <form>
+          <p>
+            <input     
+              type="text"
+              name="title"
+              placeholder="Title your note"
+              value={currentNote.title}
+              onChange={this.handleChanges}
+            />
+          </p>
+              
+          <textarea 
+            name="body"
+            value={this.props.currentNote.body}
+            onChange={this.handleChanges}
+          >
+          </textarea>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default NoteForm
