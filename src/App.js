04dd2d576697696c6dfacea5a1f4032868fe2767
Mia Rivera <riverami@iupui.edu>
2017-07-10 14:27:21 -0400

    Add google authentication via Firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import base from './base'
 
 import './App.css'
+import base, {auth} from './base'
 import Main from './Main'
 import SignIn from './SignIn'
 
@@ -66,8 +66,14 @@ class App extends Component {
     return this.state.uid
   }
 
-  handleAuth = () => {
-    this.setState({uid: 'mrivera'})
+  handleAuth = (result) => {
+    this.setState({uid: result.user.uid})
+  }
+
+  signOut = () => {
+    auth 
+      .signOut()
+      .then(() => this.setState({uid: null}))
   }
 
   render() {
