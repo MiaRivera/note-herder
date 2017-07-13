0d1c4adc997f70fa575044a9db47c2b3b5af98e2
Mia Rivera <riverami@iupui.edu>
2017-07-13 13:52:20 -0400

    Sort notelist by updatedAt

--- src/NoteList.js
+++ src/NoteList.js
@@ -1,24 +1,23 @@
-import React from 'react'
-
 import './NoteList.css'
 import Note from './Note'
 
-const NoteList = ({ notes }) => {
-  const noteIds = Object.keys(notes)
+const NoteList = () => {
+  const sortNotes = (a, b) => {
+    return (notes[b].updatedAt || 0) - notes[a].updatedAt
+  }
+
+  const noteIds = Object.keys(notes).sort(sortNotes)
 
   return (
     <div className="NoteList">
       <h3>Notes</h3>
       <ul id="notes">
         {noteIds.map(noteId => (
-          <Note
-            key={noteId}
-            note={notes[noteId]}
-          />
+          <Note key={noteId} note={notes[noteId]} />
         ))}
       </ul>
     </div>
-  )
-}
+    )
+  }
 
 export default NoteList
